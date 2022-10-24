b83ca007f97c54d80dd950deefd0b9c2904d0386
alitarakci2 <alitarakci2@gmail.com>
2022-10-24 17:20:18 +0300

    Init Class created at Dal Layer.

---
+++ Project.DAL/StrategyPattern/MyInit.cs
@@ -0,0 +1,49 @@
+﻿using Project.COMMON.Tools;
+using Project.DAL.Context;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.StrategyPattern
+{
+    public class MyInit:CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+
+            #region Admin
+
+
+
+            AppUser au = new AppUser();
+            au.UserName = "ali";
+            au.Password = DantexCrypt.Crypt("123");
+            au.Email = "alitarakci2@gmail.com";
+            au.Role = ENTITIES.Enums.UserRole.Admin;
+
+
+
+
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
